4f320847ed1b2db5a420157873e7aee366141209
murata-naoya <murata.small.baseball@gmail.com>
2019-10-11 06:38:52 +0000

    第8章

--- sample_app/app/controllers/application_controller.rb
+++ sample_app/app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
-	def hello
-		render html: "hello, world!"
-	end
+  protect_from_forgery with: :exception
+  include SessionsHelper
+
+  def hello
+    render html: 'hello, world!'
+  end
 end
