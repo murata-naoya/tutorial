4404d10d676ac541382470425196064d7b67039e
murata-naoya <murata.small.baseball@gmail.com>
2019-10-12 12:59:14 +0000

    第12章

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -6,14 +6,13 @@ class UsersController < ApplicationController
 
   def index
     #@users = User.all
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   # GET /users/:id
   def show
     @user = User.find(params[:id])
-    # => app/views/users/show.html.erb
-    # debugger
+    redirect_to root_url and return unless @user.activated?
   end
 
   # GET /users/new
@@ -27,10 +26,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save # => Validation
       # Sucess
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-      # GET "/users/#{@user.id}" => show
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       # Failure
       render 'new'
